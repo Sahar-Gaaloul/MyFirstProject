84b0d07875eca73fbb441379f62b61047f18e769
sahargaaloul <sahar.gaaloul87@gmail.com>
2020-12-19 11:33:03 +0100

    Sahar commit

--- src/main/java/com/sahar/MyFirstProjectApplication.java
+++ src/main/java/com/sahar/MyFirstProjectApplication.java
@@ -8,7 +8,7 @@ public class MyFirstProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyFirstProjectApplication.class, args);
-		// Hello
+		System.out.println ("Hello");
 	}
 
 }
